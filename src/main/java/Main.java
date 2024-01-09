e971f1e51d98eae1866eda989af6f3b601151ea3
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 17:13:34 +0330

    test # 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,22 @@
+import entities.EmployeeAddress;
+import service.EmployeeService;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        EmployeeService employeeService = new EmployeeService();
+
+/*
+        int employeeNumber = employeeService.addEmployee();
+        if (employeeNumber != -1)
+            System.out.println("employee is added with Employee Number: " + employeeNumber);
+*/
+
+        EmployeeAddress employeeAddress = employeeService.findAddress(3);
+        if (employeeAddress == null)
+            System.out.println("address not found.");
+        else
+            System.out.println(employeeAddress);
+    }
 }
