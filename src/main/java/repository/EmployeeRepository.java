13bff70eba79d1a9dbc1ffdd09238ef7f7c1060f
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 13:07:04 +0330

    Add loadAddress method

--- src/main/java/repository/EmployeeRepository.java
+++ src/main/java/repository/EmployeeRepository.java
@@ -46,5 +46,23 @@ public class EmployeeRepository {
 
         return -1;
     }
+    public EmployeeAddress loadAddress(int employeeNumber) throws SQLException {
+        String findAddress = "SELECT A.* \n" +
+                "FROM employee_address A\n" +
+                "JOIN employees E USING (address_id)\n" +
+                "WHERE E.employee_number = ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(findAddress);
+        preparedStatement.setInt(1, employeeNumber);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            int id = resultSet.getInt("address_id");
+            String city = resultSet.getString("city");
+            String street = resultSet.getString("street");
+            int postalCode = resultSet.getInt("postal_code");
+            return new EmployeeAddress(id, city, street, postalCode);
+        }
+        return null;
+    }
 
 }
