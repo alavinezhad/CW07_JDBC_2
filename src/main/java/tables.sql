9419e55cba64b6d9a8300b0def2c27d3eba25a74
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 11:14:09 +0330

    change foreign key from employee_address to employee, address_id is foreign key for employee table

--- src/main/java/tables.sql
+++ src/main/java/tables.sql
@@ -17,12 +17,18 @@ CREATE TABLE IF NOT EXISTS public.employee_address
     PRIMARY KEY (address_id)
     );
 
-ALTER TABLE IF EXISTS public.employee_address
+ALTER TABLE IF EXISTS public."Employee"
+    ADD FOREIGN KEY (address_id)
+        REFERENCES public.employee_address (address_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
+/*ALTER TABLE IF EXISTS public.employee_address
     ADD FOREIGN KEY (address_id)
     REFERENCES public.employees (address_id) MATCH SIMPLE
     ON UPDATE NO ACTION
        ON DELETE NO ACTION
         NOT VALID;
-
+*/
 INSERT INTO employees (last_name, first_name, office_code, address_id)
 VALUES ('alavi', 'hassan', 1, 123);
