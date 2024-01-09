6f6d54a3bbc787b1537fadf32e4d175c382b0687
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 10:31:37 +0330

    modify employee_number and address_id to SERIAL

--- src/main/java/tables.sql
+++ src/main/java/tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS public.employees
 (
-    employee_number integer NOT NULL,
-    last_name character varying(50) NOT NULL,
-    first_name character varying(50) NOT NULL,
+    employee_number SERIAL NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
     office_code integer NOT NULL,
-    address_id integer NOT NULL,
+    address_id integer NOT NULL UNIQUE,
     PRIMARY KEY (employee_number)
     );
 
 CREATE TABLE IF NOT EXISTS public.employee_address
 (
-    address_id integer NOT NULL,
-    city character varying(255) NOT NULL,
-    street character varying(255) NOT NULL,
+    address_id SERIAL NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
     postal_code integer NOT NULL,
     PRIMARY KEY (address_id)
     );
@@ -23,3 +23,6 @@ ALTER TABLE IF EXISTS public.employee_address
     ON UPDATE NO ACTION
        ON DELETE NO ACTION
         NOT VALID;
+
+INSERT INTO employees (last_name, first_name, office_code, address_id)
+VALUES ('alavi', 'hassan', 1, 123);
