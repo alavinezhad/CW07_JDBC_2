de28c270fa621885ecd61a2ad216835b8f9d5253
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 08:17:09 +0330

    add getter and setter

--- src/main/java/entities/EmployeeAddress.java
+++ src/main/java/entities/EmployeeAddress.java
@@ -21,4 +21,36 @@ public class EmployeeAddress {
         this.street = street;
         this.postalCode = postalCode;
     }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
 }
