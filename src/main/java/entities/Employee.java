8600aef003b510ce0754d071756331f86115f293
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 08:08:12 +0330

    add fields

---
+++ src/main/java/entities/Employee.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Employee {
+    private int employeeNumber;
+    private String lastName;
+    private String firstName;
+    private int officeCode;
+    private int addressId;
+
+}
