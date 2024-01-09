fa596bb8ea5d9404dc1893979f750f5e94be6480
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 08:21:15 +0330

    add connection field and getter method

--- src/main/java/repository/JdbcConnection.java
+++ src/main/java/repository/JdbcConnection.java
@@ -1,4 +1,18 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    private final Connection connection =
+            DriverManager.getConnection("jdbc:postgresql://localhost:5432/cw07_jdbc",
+                    "postgres", "67065018");
+
+    public JdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
