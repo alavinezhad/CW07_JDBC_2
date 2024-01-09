15d775f1b648ac0f6a041f6d2fd113107a139e20
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-09 08:10:15 +0330

    add no arg and all arg constructor

--- src/main/java/entities/Employee.java
+++ src/main/java/entities/Employee.java
@@ -7,4 +7,19 @@ public class Employee {
     private int officeCode;
     private int addressId;
 
+    public Employee() {
+    }
+
+    public Employee(int employeeNumber,
+                    String lastName,
+                    String firstName,
+                    int officeCode,
+                    int addressId) {
+        this.employeeNumber = employeeNumber;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.officeCode = officeCode;
+        this.addressId = addressId;
+    }
+
 }
