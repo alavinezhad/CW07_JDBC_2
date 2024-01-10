bbd3fcc700ef04a8a3cbbe5960744ec0248c6211
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-10 11:58:47 +0330

    small modification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,14 @@
 import entities.EmployeeAddress;
 import service.EmployeeService;
+import service.Menu;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         EmployeeService employeeService = new EmployeeService();
+        Menu menu = new Menu();
+        menu.menu();
 
-/*
-        int employeeNumber = employeeService.addEmployee();
-        if (employeeNumber != -1)
-            System.out.println("employee is added with Employee Number: " + employeeNumber);
-*/
-
-/*
-        employeeService.editOfficeCode(2);
-        employeeService.editAddress(2);
-*/
-
-/*
-        EmployeeAddress employeeAddress = employeeService.findAddress(3);
-        if (employeeAddress == null)
-            System.out.println("address not found.");
-        else
-            System.out.println(employeeAddress);
-*/
-        employeeService.deleteEmployee(2);
     }
 }
